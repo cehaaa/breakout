0cf085fa8d1ad04f8e82f7a2900b1fae4ebe0238
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2022-08-13 10:41:08 +0700

    feat: constant

---
+++ src/constant.ts
@@ -0,0 +1,7 @@
+export const app: HTMLElement = document.getElementById("app")!;
+
+export const canvas: HTMLCanvasElement = document.createElement("canvas");
+export const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+export const height: number = 400;
+export const width: number = 400;
